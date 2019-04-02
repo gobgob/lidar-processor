10dd24b89190f860683755a98fe544c02e2f901b
clemsciences <clemsciences@aol.com>
2019-04-02 23:55:33 +0200

    Retrieves lidar data

--- src/main/data_retrieval.py
+++ src/main/data_retrieval.py
@@ -1,23 +1,43 @@
 import socket
 import time
-
+from threading import Thread
 import queue
+from typing import List
+
 hote = "127.0.0.1"
 port = 17685
 
 
+def split_turn(turn: List[str]):
+    return [measure.split(":") for measure in "".join(turn).split(";")]
+
+
 def read_lidar_measures():
+    global measuring
     lidar_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lidar_socket.connect((hote, port))
     print("Connection on {}".format(port))
-    measures = queue.Queue()
-    with open("mesures.txt", "w") as f:
+    measures = queue.Queue(maxsize=10)
 
-        while True:
-            # socket.send(b"R")
-            # time.sleep(1)
-            one_message = lidar_socket.recv(100000).decode("utf-8")
-            f.write(one_message + "\n")
-            measures.put(one_message)
+    while measuring:
+        # socket.send(b"R")
+        # time.sleep(1)
+        current_measure = []
+        content = lidar_socket.recv(5000).decode("utf-8")
+        for c in content:
+            if c == 'M':
+                measures.put(split_turn(current_measure))
+                current_measure = []
+            else:
+                current_measure.append(c)
+    if not measures.empty():
+        print(measures.get())
     print("Close")
     lidar_socket.close()
+
+
+if __name__ == "__main__":
+    measuring = True
+    Thread(target=read_lidar_measures).start()
+    input("Blabla")
+    measuring = False
