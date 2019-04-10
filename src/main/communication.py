16fa8c1fb6e5672b66685f71ffa7af3df5f4a450
clemsciences <clemsciences@aol.com>
2019-04-10 22:51:28 +0200

    Docs

---
+++ src/main/communication.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+1.
+Communication with high level of the robot.
+
+2.
+Retrieves wheel encoder measures.
+Filters measures.
+
+
+"""
+
---
+++ src/main/self_locator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+At the beginning, we know the position of the robot and immobile beacons.
+
+We can follow the immobile beacons as the robot moves.
+
+If we know where the beacons are, we know where the robot is so we get a precise measure of its current position.
+"""
+
+
+
+
+
