1c10159aa7bf6b350450e10d0d70a994d8ab7e95
clement_besnier <clemsciences@aol.com>
2019-04-18 18:07:18 +0200

    Added Segment class to check if an obstacle point is visible from the robot or not.

--- src/main/table.py
+++ src/main/table.py
@@ -2,6 +2,8 @@
 Table with LiDAR obstacles.
 """
 
+from __future__ import annotations
+
 from typing import List
 
 import numpy as np
@@ -19,13 +21,13 @@ class Point:
         return Point(self.x + other.x, self.y + other.y)
 
     def multiply_by(self, a):
-        return Point(a*self.x, a*self.y)
+        return Point(a * self.x, a * self.y)
 
     def distance(self, other):
-        return np.sqrt(np.power(self.x - other.x, 2)+np.power(self.y - other.y, 2))
+        return np.sqrt(np.power(self.x - other.x, 2) + np.power(self.y - other.y, 2))
 
     def __str__(self):
-        return "("+str(self.x)+","+str(self.y)+")"
+        return "(" + str(self.x) + "," + str(self.y) + ")"
 
     def rotate(self, angle: float):
         """
@@ -55,17 +57,17 @@ class Vector:
         self.y = point2.y - point1.y
 
     def compute_angle(self):
-        return np.arctan(self.y/self.x)
+        return np.arctan(self.y / self.x)
 
     def compute_distance(self):
-        return np.sqrt(self.x**2 + self.y**2)
+        return np.sqrt(self.x ** 2 + self.y ** 2)
 
     def apply_to_point(self, point: Point):
         print(point)
-        return Point(point.x+self.x, point.y+self.y)
+        return Point(point.x + self.x, point.y + self.y)
 
     def __str__(self):
-        return str(self.x)+" - "+str(self.y)
+        return str(self.x) + " - " + str(self.y)
 
     def set_coordinates(self, x: float, y: float):
         self.x = x
@@ -89,6 +91,43 @@ class Vector:
         self.y = res[1]
 
 
+class Segment:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+        self.x_difference = self.compute_x_difference()
+        self.y_difference = self.compute_y_difference()
+        self.determinant = self.compute_determinant()
+
+    def compute_determinant(self):
+        return self.p1.x * self.p2.y - self.p1.y * self.p2.x
+
+    def compute_x_difference(self):
+        return self.p1.x - self.p2.x
+
+    def compute_y_difference(self):
+        return self.p1.y - self.p2.y
+
+    def get_determinant(self):
+        return self.determinant
+
+    def get_x_difference(self):
+        return self.x_difference
+
+    def get_y_difference(self):
+        return self.y_difference
+
+    def collide(self, other: Segment):
+        denominator = (self.get_x_difference() * other.get_y_difference() -
+                       self.get_y_difference() * other.get_x_difference())
+        px = (self.get_determinant() * other.get_x_difference() - self.get_x_difference() * other.get_determinant()) / \
+            denominator
+
+        py = (self.get_determinant() * other.get_y_difference() - self.get_y_difference() * other.get_determinant()) / \
+            denominator
+        pass
+
+
 class Obstacle:
     def __init__(self, center: Point):
         self.center = center
@@ -99,11 +138,12 @@ class Square(Obstacle):
         center = Point(0, 0)
         for position in positions:
             center = center + position
-        center = center.multiply_by(1/4.)
+        center = center.multiply_by(1 / 4.)
 
         super().__init__(center)
 
         self.positions = positions
+        assert len(positions) == 4
 
     def compute_projection_on(self, point: Point):
         # TODO compute projection of a point on the obstacle
@@ -138,6 +178,12 @@ class Square(Obstacle):
             positions.append(point)
         return Square(positions)
 
+    def check_visible_points(self, robot_point: Point):
+        vertices = []
+        # vertices.extend([ for obstacle in self.o])
+        #         # for vertex in self.positions:
+        #         #     pass
+
 
 class Table:
     def __init__(self):
@@ -154,7 +200,7 @@ class Table:
     def add_edge_point(self, edge: Point):
         self.edges.append(edge)
 
-    def init_plot(self,):
+    def init_plot(self, ):
         self.fig = pl.figure()
         ax = self.fig.add_subplot(111)
         ax.clear()
@@ -204,7 +250,7 @@ class Table:
             print(vec)
 
         robot_vector = Vector()
-        robot_vector.set_coordinates(np.cos(angle)*rho, np.sin(angle)*rho)
+        robot_vector.set_coordinates(np.cos(angle) * rho, np.sin(angle) * rho)
 
         return vectors, robot_vector
 
@@ -228,15 +274,30 @@ class Table:
         self.obstacles = [obstacle.rotate(angle) for obstacle in self.obstacles]
         self.edges = [edge_point.rotate(angle) for edge_point in self.edges]
 
+    def generate_measures(self, robot_point: Point):
+        vertices = []
+        edges = []
+        for obstacle in self.obstacles:
+            for i in range(len(obstacle.positions) - 1):
+                vertices.append(obstacle.positions[i])
+                edges.append((obstacle[i], obstacle[i + 1]))
+            edges.append((obstacle.positions[-1], obstacle.positions[0]))
+
+        # invisible_vertices = []
+        # for vertex in vertices:
+        #     for edge in edges:
+        #         if
+        #         invisible_vertices.append(vertex)
+
 
 def main():
     table = Table()
     # for orange
-    beacon_1 = Square([Point(-1500-100, 2000), Point(-1500, 2000), Point(-1500, 2000-100),
-                       Point(-1500-100, 2000-100)])
-    beacon_2 = Square([Point(1500, 1000+50), Point(1500+100, 1000+50), Point(1500+100, 1000-50),
-                       Point(1500, 1000-50)])
-    beacon_3 = Square([Point(-1500-100, 0+100), Point(-1500, 0+100), Point(-1500, 0), Point(-1500-100, 0)])
+    beacon_1 = Square([Point(-1500 - 100, 2000), Point(-1500, 2000), Point(-1500, 2000 - 100),
+                       Point(-1500 - 100, 2000 - 100)])
+    beacon_2 = Square([Point(1500, 1000 + 50), Point(1500 + 100, 1000 + 50), Point(1500 + 100, 1000 - 50),
+                       Point(1500, 1000 - 50)])
+    beacon_3 = Square([Point(-1500 - 100, 0 + 100), Point(-1500, 0 + 100), Point(-1500, 0), Point(-1500 - 100, 0)])
 
     table.add_square_obstacle(beacon_1)
     table.add_square_obstacle(beacon_2)
@@ -287,7 +348,7 @@ def main_2():
     measure = Point(-1000, 1100)
     measure = translation_vector.apply_to_point(measure)
     measure.rotate(rotation_angle)
-    vectors, robot_vector = table.simulate_measure(measure, 0.5, 200)
+    vectors, robot_vector = table.simulate_measure(measure, 0, 200)
 
     table.init_plot()
     table.plot_edges()
