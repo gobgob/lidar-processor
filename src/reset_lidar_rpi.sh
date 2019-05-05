e11d98fd6e07c2dc0eea915f57067cb7ffc09c5e
clemsciences <clemsciences@aol.com>
2019-05-05 18:12:58 +0200

    Scripts to send code to Raspberry-PI

---
+++ src/reset_lidar_rpi.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rm -r /home/pi/lidar-processor/src/main/
+mkdir -p /home/pi/lidar-processor/src/main/
---
+++ src/send_to_rpi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp src/main/* pi@172.24.1.1:/home/pi/lidar-processor/
+
