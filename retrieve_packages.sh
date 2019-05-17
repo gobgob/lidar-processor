e7a4410d770c10a6d2141b77ddd81102250b15cb
Clément Besnier <clemsciences@aol.com>
2019-05-17 23:58:12 +0200

    script to install Python 3.7 tested

--- retrieve_packages.sh
+++ retrieve_packages.sh
@@ -8,22 +8,25 @@
 download_python37()
 {
     # for Python 3.7
-    apt-get download build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    #apt-get download build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev \
         libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
     wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
 }
+#download_python37
 install_python37()
 {
     # on target
-    dpkg -i download build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-        libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+    #idpkg -i *.deb
+    #dpkg -i build-essential* checkinstall* libreadline-gplv2-dev* libncursesw5-dev* libssl-dev* \
+    #    libsqlite3-dev* tk-dev* libgdbm-dev* libc6-dev* libbz2-dev* libffi-dev* zlib1g-dev*
     tar xzf Python-3.7.3.tgz
 
     cd Python-3.7.3
     sudo ./configure --enable-optimizations
     sudo make altinstall
 }
+install_python37
 
 install_venv_python37()
 {
