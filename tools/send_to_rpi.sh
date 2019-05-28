946acb63613e438171189459f535bba4d231f619
clemsciences <clemsciences@aol.com>
2019-05-28 10:59:13 +0200

    Useful commands

--- tools/send_to_rpi.sh
+++ tools/send_to_rpi.sh
@@ -4,3 +4,9 @@ scp src/main/* pi@172.24.1.1:/home/pi/lidar-processor/
 
 scp retrieve_packages.sh pi@172.24.1.1:/home/pi/lidar-processor/
 # scp Python-3.7.3.tgz pi@172.24.1.1:/home/pi/lidar-processor/
+
+
+
+# From /home/pi/lidar-processor
+scp -r src/main_script.py pi@172.24.1.1:/home/pi/lidar-processor/src/
+scp -r src/main pi@172.24.1.1:/home/pi/lidar-processor/src/
