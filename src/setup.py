fda6e0272f9fc4124f141d8feea26fcc07d0707b
clement_besnier <clemsciences@aol.com>
2019-06-07 16:14:32 +0200

    New setup.py file.

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lidarproc',
+    version='1.0.0',
+    packages=['data', 'main', 'scripts'],
+    package_dir={'': 'src'},
+    url='https://www.clementbesnier.fr/projets/cdr2019',
+    license='',
+    author='Clément Besnier',
+    author_email='clemsciences@aol.com',
+    description=''
+)
