909d9c366aa415897a424fd8034bb7d10028a0b8
clemsciences <clemsciences@aol.com>
2019-06-10 00:53:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import find_packages
 setup(
     name='lidarproc',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['docs', 'samples', 'logs']),
     url='https://www.clementbesnier.fr/projets/cdr2019',
-    license='',
+    license='LICENSE.txt',
     author='Clément Besnier',
     author_email='clemsciences@aol.com',
-    description=''
+    description='Code used by GobGob Senpai during the France robotics cup 2019',
+    long_description=open("README.md").read(),
+    install_requires=['matplotlib', 'numpy', 'scipy', 'scikit-image'],
 )
