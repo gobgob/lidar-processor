d800bf3ec95f922e2159c7dee5593d2eeb4efd48
Clément Besnier <clemsciences@aol.com>
2019-09-24 21:49:59 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-from setuptools import setup
-from setuptools import find_packages
+
+import io
+import os
+from setuptools import setup, find_packages
+
+DEPENDENCIES = ["matplotlib", "numpy", "scipy", "scikit-image"]
+EXCLUDE_FROM_PACKAGES = ['docs', 'samples', 'logs']
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
+    README = f.read()
 
 setup(
     name='lidarproc',
     version='1.0.0',
-    packages=find_packages(exclude=['docs', 'samples', 'logs']),
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data= True,
     url='https://www.clementbesnier.fr/projets/cdr2019',
-    license='LICENSE.txt',
+    keywords=[],
+    scripts=[],
+    zip_safe=False,
+    license='License :: OSI Approved :: MIT License',
     author='Clément Besnier',
     author_email='clemsciences@aol.com',
+    install_requires=DEPENDENCIES,
     description='Code used by GobGob Senpai during the France robotics cup 2019',
     long_description=open("README.md").read(),
-    install_requires=['matplotlib', 'numpy', 'scipy', 'scikit-image'],
 )
